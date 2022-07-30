792611865bc10c02b5fd91c40459d267c3771bc3
Fpsska <fpsska1337@gmail.com>
2022-07-30 14:20:28 +0300

    feat: create table.controller.js file, relocate logic of createTableDataItem, getTableData from table.routes.js into this file

--- routes/table.routes.js
+++ routes/table.routes.js
@@ -1,15 +1,14 @@
 const { Router } = require('express');
-const db = require('../db');
 
 const router = Router();
+const tableController = require('../controllers/table.controller');
 
-router.get('/', async (req, res) => {
-    try {
-        const alLData = await db.query('SELECT * FROM mockdata');
-        res.json(alLData.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
+
+
+router.post('/', tableController.createTableDataItem);
+router.get('/', tableController.getTableData);
+
+
+
 
 module.exports = router;
