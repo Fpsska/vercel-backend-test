a0d54bab8c201c216a96404e88175cadfee3de75
Fpsska <fpsska1337@gmail.com>
2022-07-31 13:42:36 +0300

    refactor: change pool configuration options

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'ADMIN',
-    host: 'localhost',
+    user: 'jwagicgk',
+    password: '9dkNX4FaZTp9FaypHqAjktVntKTQR3Y2',
+    host: 'manny.db.elephantsql.com',
     port: 5432,
-    database: 'welbex_db'
+    database: 'jwagicgk'
 })
 
 module.exports = pool;
